32d0991db9d678fed077237a457f23549150f6b8
karsh <utkarsh@cyberfear.com>
2024-10-20 09:11:40 +1100

    organise
    - removed tailing whitespaces
    ~ updated example usage
    ~ fixed directory change, $PLAYGROUND

--- devtest.zsh
+++ devtest.zsh
@@ -1,57 +1,57 @@
 
-#- DevTest ---------------------------------------------------------------------
+#- Dev Bench -------------------------------------------------------------------
 #
-# ZSH plugin to create temporary "test" directories in /tmp/ to test things out.
+# ZSH plugin to create temporary "" directories in /tmp/ to test things out.
 # With no argument, tst command will create an empty directory labled /tmp/<NUM>
-# where NUM would be test index.
+# where NUM would be  index.
 #
-# With a numerical argument, the command will check if that indexed test
+# With a numerical argument, the command will check if that indexed
 # directory exists, if it does then it would cd there, else it would print
 # error.
 #
 # With language .extension argument, it will use $PROJ_INIT script to create a
-# template at /tmp/test<NUM> for that language. An example script for this
+# template at /tmp/<NUM> for that language. An example script for this
 # purpose can be grabbed from my github-
 # https://github.com/karshPrime/SysHacks/blob/main/project_initialise.sh
 #
 # example usage:
 # $ tst
-# [ checks if /tmp/test1 exists, if it doesn't create it, else check for test2 ]
-# [ create /tmp/test? & cd /tmp/test? ]
+# [ checks if /tmp/1 exists, if it doesn't create it, else check for test2 ]
+# [ create /tmp/? & cd /tmp/test? ]
 #
-# $ tst 1
-# [ checks if /tmp/test1 exists, if it does, cd there ]
+# $ bench 1
+# [ checks if /tmp/1 exists, if it does, cd there ]
 #
-# $ tst c
-# [ checks if /tmp/test1 exists, if it doesn't create it, else check for test2 ]
-# [ uses $PROJ_INIT to generate C language project template at /tmp/test? ]
-# 
-# $ tstrm
-# [ if $pwd in /tmp/test? then cd ~ ]
-# [ deletes all /tmp/test* ]
+# $ bench c
+# [ checks if /tmp/1 exists, if it doesn't create it, else check for test2 ]
+# [ uses $PROJ_INIT to generate C language project template at /tmp/? ]
+#
+# $ benchcl
+# [ if $pwd in /tmp/? then cd ~ ]
+# [ deletes all /tmp/* ]
 #
 #-------------------------------------------------------------------------------
 
 # Path for project_initialise script
 PROJ_INIT="$HACK_SCRIPTS/project_initialise.sh"
-TEST_DIR="$HOME/Projects/.test"
+PLAYGROUND="$HOME/Projects/.Playground"
 
 # Find the last test directory
 bench () {
     # act locally, if first arg is .
 	if [[ "$1" == "." ]]
 	then
-		BENCHES="." 
+		BENCHES="."
 		shift
 	else
-		BENCHES=$PLAYGROUND 
+		BENCHES=$PLAYGROUND
 	fi
 
     # if argument is a number, check if that indexed test dir already exists
 	# if it does exist then move there
 	if [[ "$1" =~ ^[0-9]+$ ]]
 	then
-		target="$BENCHES/bench$1" 
+		target="$BENCHES/bench$1"
 		if [ -d "$target" ]
 		then
 			cd "$target" || return
@@ -68,16 +68,16 @@ bench () {
         grep -o '[0-9]*$' |
         sort -n |
         tail -1
-    ) 
+    )
 
 	# if no directory found, set the highest to 0
 	if [ -z "$highest" ]
 	then
-		highest=0 
+		highest=0
 	fi
 
 	# increment the highest directory number
-	next=$((highest + 1)) 
+	next=$((highest + 1))
 	if [ -n "$1" ]
 	then
 		if ! "$PROJ_INIT" "bench$next.$1"
@@ -95,12 +95,12 @@ bench () {
 # Delete all test directories with self remove utility
 benchcl () {
 	setopt RM_STAR_SILENT
-	local cwd=$(pwd) 
+	local cwd=$(pwd)
 	if [[ "$1" == "." ]]
 	then
-		BENCHES="." 
+		BENCHES="."
 	else
-		BENCHES=$PLAYGROUND 
+		BENCHES=$PLAYGROUND
 	fi
 	if [[ $cwd == "$BENCHES"* ]]
 	then
